f69013ddae2ff6876d689910b8fcdf15046f9ab2
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-05-28 01:27:02 +0000

    Receiving data from  API

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const API_KEY = "koD1zeePAkm6ZaaJ-hIp14WyGxc";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
