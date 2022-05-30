060c50d0b82214d5812f22fa6a88a98613e0653e
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-05-30 09:48:00 +0000

    Display error  exceptions in a modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,8 +40,22 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayExceptions(data);
         throw new Error(data.error);
     } 
+}
+
+function displayExceptions(data) {
+    
+    let heading = `An Exceptions Occured`;
+    results = `<div>The API returned status code ${data.status_code}</div>`
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`
+    results += `<div>Error text: <strong>${data.error}</strong></div>`
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show()
 }
 
 function displayErrors(data) {
